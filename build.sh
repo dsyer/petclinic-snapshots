cd1156527eb94f2f0b0b199310e1bea888c95a0b
Dave Syer <dsyer@pivotal.io>
2020-05-09 13:40:08 +0100

    Tweak build script

--- build.sh
+++ build.sh
@@ -29,6 +29,10 @@ if ! [ -f buildroot/overlay/root/petclinic.jar ]; then
 fi
 
 if ! [ -f buildroot/output/images/rootfs.ext2 ]; then
+    if ! [ `id -u` == "1000" ]; then
+        echo Not running as user uid=1000. Aborting.
+        exit 3
+    fi
     # This works if you run as user uid=1000
     docker run -v `pwd`/buildroot:/home/br-user/buildroot -w /home/br-user/buildroot buildroot/base make
 fi
@@ -37,7 +41,10 @@ if ! [ -f buildroot/output/images/rootfs.qcow ]; then
     (cd buildroot/output/images/;  qemu-img convert -O qcow2 rootfs.ext2 rootfs.qcow)
 fi
 
-(cd buildroot/output/images; qemu-system-x86_64 -M pc-i440fx-2.8 -enable-kvm -m 2048 -kernel bzImage -drive file=rootfs.qcow,if=virtio,format=qcow2 -append 'rootwait root=/dev/vda console=ttyS0' -net nic,model=virtio -net user,hostfwd=tcp::2222-:22,hostfwd=tcp::8080-:8080,hostfwd=tcp::45455-:45455,id=net0 -nographic -monitor telnet::45454,server,nowait) &
+(cd buildroot/output/images; qemu-system-x86_64 -M pc-i440fx-2.8 -enable-kvm -m 2048 -kernel bzImage \
+    -drive file=rootfs.qcow,if=virtio,format=qcow2 -append 'rootwait root=/dev/vda console=ttyS0' \
+    -net nic,model=virtio -net user,hostfwd=tcp::8080-:8080 \
+    -nographic -monitor telnet::45454,server,nowait) &
 
 while ! curl -s localhost:8080 2>&1 > /dev/null; do
 	sleep 0.01
